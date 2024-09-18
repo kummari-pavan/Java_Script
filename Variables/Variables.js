bf61b409fbe92fcbb24ec0e489f7d64e4c19a056
kummari-pavan <kpavan892002@gmail.com>
2024-09-18 14:25:17 +0530

    [PAVAN]ADD:Variables

---
+++ Variables/Variables.js
@@ -0,0 +1,18 @@
+//Var 
+if (true) {
+    var a = 5; 
+  }
+  console.log(a); 
+
+//Let
+if (true) {
+    let b = 10; 
+  }
+  console.log(b);
+
+//Const
+if (true) {
+    const c = 15; // `c` is block-scoped and cannot be reassigned
+    // c = 20; // Error: Cannot reassign a `const` variable
+  }
+  console.log(c);
