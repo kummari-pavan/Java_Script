60b5fbdcd638cc876fbba602c4d05c5906bf2f31
kummari-pavan <kpavan892002@gmail.com>
2024-09-18 14:19:30 +0530

    [PAVAN]Add:Data Types Files

---
+++ Data_Types/DataTypes.js
@@ -0,0 +1,21 @@
+//Premitive Data Types 
+
+//Strings
+let name = "John";
+
+//Number
+let age = 25;
+let price = 99.99; 
+
+//Boolean
+let isOnline = true;
+
+//Undefined
+let x;
+console.log(x); 
+
+//Null
+let person = null;
+
+//BigInt
+let largeNumber = BigInt(1234567890123456789012345678901234567890n);
